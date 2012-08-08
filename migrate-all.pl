cbc84431be9282059336242215c3a688242e5668
wylde780 <wylde780@gmail.com>
2012-08-08 16:51:53 -0400

    Added storage checking

--- migrate-all.pl
+++ migrate-all.pl
@@ -6,13 +6,22 @@ chomp($dst_mig = $ARGV[1]);
 
 #@src_node = `pvesh get /nodes/$ARGV[0] 2>&1`; 
 chomp(@src_vms = `ssh $ARGV[0] qm list | grep running`);
+chomp(@storage = `pvesh get /storage 2>&1 | grep storage`);
+for($x=0; $x<@storage; $x++)
+{
+	if($storage[$x] =~ m/local/) 
+	{
+		splice @storage, $x, 1;
+	}	
+}
+print @storage;
 %vmlist =();
 $count = 0;
 foreach(@src_vms)
 {
-	$_ =~ s/ +/,/g;
-       	$_ =~ s/ /,/g;
-       	$_ =~ s/^,//;
+	$_ =~ s/ +/,/g; 
+	$_ =~ s/ /,/g; 
+	$_ =~ s/^,//;
 	@tmp = split(",",$_);	
 	$vmlist{$count}{"vmid"} = $tmp[0];
 	$vmlist{$count}{"max-mem"} = $tmp[3];
@@ -30,21 +39,28 @@ sub CheckDisk()
 		@conf = `ssh $src_mig cat $conf`;
 		foreach(@conf)
 		{
-			if(/local/)
+			if(/local:/)
 			{
-				#ide0: local:210/vm-210-disk-2.qcow2
-				@tmp = split('\/', $_);
-				print "VMID -> $vmlist{$x}{vmid}\nHostname -> $vmlist{$x}{hostname}\nhas a local disk "
-					. "and has been removed from migration\n";
-				print "Match $tmp[1]\n";
+				chomp(@tmp = split('\/', $_));
+				print "VMID -> $vmlist{$x}{vmid}\nHostname -> $vmlist{$x}{hostname}\n"
+					. "Disk -> $tmp[1]\n"
+					. "has a local disk and has been removed from the migration list\n";
 				delete($vmlist{$x});
 			}
-		&migrate;			
+#			if(
 		}
-	}	
+	}
+&migrate(@migrate);	
 }#End CheckDisk 
 sub migrate()
 {
 	#Do Stuff
+	foreach(@migrate)
+	{
+		print "$x Migrating $_ from $src_mig -> $dst_mig\n";
+		
+	}
 }
 #print Dumper %vmlist;
+$blah = @migrate;
+print $blah;
